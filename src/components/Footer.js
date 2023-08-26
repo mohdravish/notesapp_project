9e419e9ebe3f5c486a5d7df5d48554408f917cdf
mohdravish <66007634+mohdravish@users.noreply.github.com>
2023-08-26 14:48:44 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='footer'>
+                <ul>
+                    <li>Notepad</li>
+                    <li>Developed by </li>
+                </ul>
+            </div>
+        </footer>
+
+    )
+}
+export default Footer;
